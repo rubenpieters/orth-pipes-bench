56794a2a5b45c52426e80cd3d026eb097fa7d48f
rubenpieters <rubenpietersgithub@gmail.com>
2020-02-26 14:57:22 +0100

    update pipe/seq benchmark figure to log y axis

--- scripts/plot_spivey_pipe.gnu
+++ scripts/plot_spivey_pipe.gnu
@@ -4,9 +4,9 @@ set autoscale y
 
 MAX=GPVAL_X_MAX
 
-set xrange [1:MAX+MAX*0.05]
+set xrange [1000:MAX+MAX*0.05]
 
-set logscale x
+set logscale xy
 
 set terminal eps enhanced font 'Verdana,10'
 set output 'benchmark_pipe.eps'
@@ -17,7 +17,7 @@ set pointsize 0.5
 
 set key left top
 set xlabel "number of deep-pipe calls [log] (n)"
-set ylabel "time (seconds)"
+set ylabel "time [log] (seconds)"
 set grid ytics
 
 plot 'results-spivey/deep-pipe_conduit.tmp' using 1:2:3:4 with errorlines title "conduit" pt 6 dt 1 lw 3 lc rgb '#3e9651', 'results-spivey/deep-pipe_pipes.tmp' using 1:2:3:4 with errorlines title "pipes" pt 4 dt 2 lw 3 lc rgb '#cc2529', 'results-spivey/deep-pipe_proxyrep.tmp' using 1:2:3:4 with errorlines title "proxyrep" pt 2 dt 3 lw 3 lc rgb '#396ab1', 'results-spivey/deep-pipe_contpipe.tmp' using 1:2:3:4 with errorlines title "contpipe" pt 8 dt 4 lw 3 lc rgb '#6b439a'
--- src/Benchmarks/Spivey.hs
+++ src/Benchmarks/Spivey.hs
@@ -40,15 +40,15 @@ criterion = defaultMain
   , createBenchIO "proxyrep" "primes" l1 OrthPipes.runPrimes
   , createBenchIO "contpipe" "primes" l1 (ContPipe.run "primes2")
   , createBenchIO "streamly" "primes" l1 StreamlyBench.runPrimes
-  , createBenchIO "pipes" "deep-pipe" (Prelude.take 4 l2) PipesBench.runDeepPipe
-  , createBenchIO "conduit" "deep-pipe" (Prelude.take 4 l2) ConduitBench.runDeepPipe
+  , createBenchIO "pipes" "deep-pipe" (Prelude.take 5 l2) PipesBench.runDeepPipe
+  , createBenchIO "conduit" "deep-pipe" (Prelude.take 5 l2) ConduitBench.runDeepPipe
   , createBenchIO "proxyrep" "deep-pipe" l2 OrthPipes.runDeepPipe
   , createBenchIO "contpipe" "deep-pipe" l2 (ContPipe.run "par2")
-  , createBenchIO "pipes" "deep-seq" (Prelude.take 4 l2) PipesBench.runDeepSeq
+  , createBenchIO "pipes" "deep-seq" (Prelude.take 5 l2) PipesBench.runDeepSeq
   , createBenchIO "conduit" "deep-seq" l2 ConduitBench.runDeepSeq
   , createBenchIO "proxyrep" "deep-seq" l2 OrthPipes.runDeepSeq
   , createBenchIO "contpipe" "deep-seq" l2 (ContPipe.run "seq2")
   ]
   where
-    l1 = [1, 1000, 2500, 5000, 7500, 10000]
-    l2 = [1, 1000, 2500, 5000, 7500, 10000, 25000, 50000, 75000, 100000, 250000, 500000]
+    l1 = [1000, 2500, 5000, 7500, 10000]
+    l2 = [1000, 2500, 5000, 7500, 10000, 25000, 50000, 75000, 100000, 250000, 500000]
